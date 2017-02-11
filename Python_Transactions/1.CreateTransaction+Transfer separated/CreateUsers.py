d9583f759dd52680e444421f959bb3924a015cc9
bluetrails <cedric@bluetrails.nl>
2017-02-11 20:48:57 +0100

    Create Article + transfer to 2 universities

--- Python_Transactions/1.CreateTransaction+Transfer separated/CreateUsers.py
+++ Python_Transactions/1.CreateTransaction+Transfer separated/CreateUsers.py
@@ -2,9 +2,9 @@ from bigchaindb_driver import BigchainDB
 from bigchaindb_driver.crypto import generate_keypair
 
 # declare users
-alice = generate_keypair()
-bob = generate_keypair()
-amit = generate_keypair()
+madscientist = generate_keypair()
+uniamsterdam = generate_keypair()
+ucl = generate_keypair()
 
 
 
---
+++ Python_Transactions/2.CreateArticle+Transferto2Universities/CreateUsers.py
@@ -0,0 +1,12 @@
+from bigchaindb_driver import BigchainDB
+from bigchaindb_driver.crypto import generate_keypair
+
+# declare users
+madscientist = generate_keypair()
+uniamsterdam = generate_keypair()
+ucl = generate_keypair()
+
+
+
+
+
